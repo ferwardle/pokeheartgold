3f85d480d3380980291a9db6e602f72775e13944
ferwardle <fredymedawar.fm23@gmail.com>
2026-06-05 14:26:43 -0500

    Clean up unk_02078834

--- include/unk_02078834.h
+++ include/unk_02078834.h
@@ -2,6 +2,8 @@
 #define POKEHEARTGOLD_UNK_02078834_H
 
 #include "task.h"
+#include "overlay_44.h"
+#include "overlay_44_02232E9C.h"
 
 typedef struct UnkStruct_sub_02078C74 {
     u8 unk0[0x34];
@@ -17,34 +19,15 @@ typedef struct UnkStruct_sub_02078C18 {
     u8 unk39;
 } UnkStruct_sub_02078C18;
 
-typedef struct UnkStruct_sub_02078B9C {
-    u8 unk0;
-    u8 unk1;
-    u8 unk2;
-    u8 unk3;
-    u8 unk4;
-    SaveData* unk8;
-} UnkStruct_sub_02078B9C;
-
-typedef struct UnkStruct_sub_02079352 {
-    SaveData* unk0;
-    u32 unk4;
-    u32 unk8;
-} UnkStruct_sub_02079352;
-
 typedef struct UnkStruct_sub_02078834 {
-    UnkStruct_sub_02079352* unk0;
+    UnkStruct_ov44_0222A758_args* overlayArgs0;
     u32 unk4;
     u16* unk8;
     u8 unkC;
     u8 unkD;
-    void* unk10;
+    void* overlayArgs1;
     u32 unk14;
 } UnkStruct_sub_02078834;
-
-typedef struct UnkStruct_sub_02078D24 {
-    u8 unk0[10][36];
-} UnkStruct_sub_02078D24;
 
 BOOL sub_02078834(TaskManager* taskManager);
 UnkStruct_sub_02078834* sub_02078B2C(void);
--- src/unk_02078834.c
+++ src/unk_02078834.c
@@ -1,18 +1,18 @@
 #include "unk_02078834.h"
-#include "unk_02037C94.h"
-#include "unk_020915B0.h"
-#include "unk_020968B0.h"
+
+#include "encounter.h"
 #include "field_system.h"
-#include "overlay_manager.h"
+#include "launch_application.h"
 #include "overlay_13_thumb_1.h"
-#include "overlay_44.h"
 #include "overlay_91.h"
 #include "overlay_92.h"
 #include "overlay_93_thumb_1.h"
-#include "encounter.h"
+#include "overlay_manager.h"
 #include "save_vars_flags.h"
 #include "sys_flags.h"
-#include "launch_application.h"
+#include "unk_02037C94.h"
+#include "unk_020915B0.h"
+#include "unk_020968B0.h"
 
 FS_EXTERN_OVERLAY(OVY_90);
 FS_EXTERN_OVERLAY(OVY_91);
@@ -20,8 +20,7 @@ FS_EXTERN_OVERLAY(OVY_92);
 FS_EXTERN_OVERLAY(OVY_93);
 FS_EXTERN_OVERLAY(OVY_44);
 
-const u8 _0210125C[] = {0x03, 0x04, 0x04, 0x04};
-
+const u8 _0210125C[] = { 3, 4, 4, 4 };
 const OverlayManagerTemplate _02101280 = {
     .init = ov44_02232EA8,
     .exec = ov44_02232F64,
@@ -53,120 +52,120 @@ const OverlayManagerTemplate _021012A0 = {
     .ovy_id = FS_OVERLAY_ID(OVY_93)
 };
 
-BOOL sub_02078834(TaskManager* taskManager) {
-    FieldSystem* fieldSystem = TaskManager_GetFieldSystem(taskManager);
-    UnkStruct_sub_02078834* temp_r0 = TaskManager_GetEnvironment(taskManager);
+BOOL sub_02078834(TaskManager *taskManager) {
+    FieldSystem *fieldSystem = TaskManager_GetFieldSystem(taskManager);
+    UnkStruct_sub_02078834 *temp_r0 = TaskManager_GetEnvironment(taskManager);
     switch (temp_r0->unk4) {
-    case 0: 
-        temp_r0->unk0->unk0 = fieldSystem->saveData;
-    case 1: 
+    case 0:
+        temp_r0->overlayArgs0->unk0 = fieldSystem->saveData;
+    case 1:
         temp_r0->unk4++;
-        if ((temp_r0->unk0->unk4 == 1) && (sub_0203A05C(fieldSystem->saveData) != 0)) {
-            temp_r0->unk4 = 0xAU;
+        if ((temp_r0->overlayArgs0->unk4 == 1) && (sub_0203A05C(fieldSystem->saveData) != 0)) {
+            temp_r0->unk4 = 10;
             temp_r0->unk8[0] = 0;
         }
         break;
-    case 2: 
-        CallApplicationAsTask(taskManager, &_02101290, temp_r0->unk0);
+    case 2:
+        CallApplicationAsTask(taskManager, &_02101290, temp_r0->overlayArgs0);
         temp_r0->unk4++;
         break;
-    case 3: 
+    case 3:
         if (sub_0203A05C(fieldSystem->saveData) != 0) {
             SetFlag970(Save_VarsFlags_Get(fieldSystem->saveData));
         }
-        switch (temp_r0->unk0->unk4) {
+        switch (temp_r0->overlayArgs0->unk4) {
         case 3:
             temp_r0->unkC = 0;
             temp_r0->unkD = 0;
             temp_r0->unk4 = 4;
             break;
         case 1:
-            temp_r0->unkC = 0x32U;
+            temp_r0->unkC = 50;
             temp_r0->unkD = 0;
             temp_r0->unk4 = 4;
             break;
         case 2:
-            temp_r0->unkC = 0x64U;
+            temp_r0->unkC = 100;
             temp_r0->unkD = 0;
             temp_r0->unk4 = 4;
             break;
         case 6:
             temp_r0->unkC = 0;
-            temp_r0->unkD = 1U;
+            temp_r0->unkD = 1;
             temp_r0->unk4 = 4;
             break;
         case 4:
-            temp_r0->unkC = 0x32U;
-            temp_r0->unkD = 1U;
+            temp_r0->unkC = 50;
+            temp_r0->unkD = 1;
             temp_r0->unk4 = 4;
             break;
         case 5:
-            temp_r0->unkC = 0x64U;
-            temp_r0->unkD = 1U;
+            temp_r0->unkC = 100;
+            temp_r0->unkD = 1;
             temp_r0->unk4 = 4;
             break;
         case 7:
-            temp_r0->unk4 = 6U;
+            temp_r0->unk4 = 6;
             break;
         case 10:
             temp_r0->unk8[0] = 1;
-            temp_r0->unk4 = 0xBU;
+            temp_r0->unk4 = 11;
             break;
         case 8:
             temp_r0->unk4 = 9;
             break;
         case 12:
-            temp_r0->unk4 = 0xCU;
+            temp_r0->unk4 = 12;
             break;
         case 13:
-            temp_r0->unk4 = 0xEU;
+            temp_r0->unk4 = 14;
             break;
         case 14:
-            temp_r0->unk4 = 0x12U;
+            temp_r0->unk4 = 18;
             break;
         case 15:
-            temp_r0->unk4 = 0x16U;
+            temp_r0->unk4 = 22;
             break;
         case 9:
-            temp_r0->unk4 = 8U;
+            temp_r0->unk4 = 8;
             break;
         }
         break;
-    case 4: 
-        CallTask_02050960(taskManager, temp_r0->unk0->unk8, temp_r0->unkC, temp_r0->unkD);
+    case 4:
+        CallTask_02050960(taskManager, temp_r0->overlayArgs0->unk8, temp_r0->unkC, temp_r0->unkD);
         temp_r0->unk4++;
         break;
-    case 5: 
+    case 5:
         temp_r0->unk4 = 2;
         break;
-    case 6: 
+    case 6:
         CallTask_WirelessTrade(taskManager);
         temp_r0->unk4++;
         break;
-    case 7: 
+    case 7:
         temp_r0->unk4 = 2;
         break;
-    case 8: 
+    case 8:
         Heap_Create(HEAP_ID_3, HEAP_ID_53, 0x40100);
         LoadOVY13();
         ov13_0221BA00(HEAP_ID_53);
         OS_ResetSystem(0);
         break;
-    case 9: 
+    case 9:
     case 10:
     case 11:
-        Heap_Free(temp_r0->unk0);
+        Heap_Free(temp_r0->overlayArgs0);
         Heap_Free(temp_r0);
         temp_r0->unk4++;
         return 1;
     case 12:
         sub_020378E4(0);
-        temp_r0->unk10 = sub_020968B0(fieldSystem, NULL);
+        temp_r0->overlayArgs1 = sub_020968B0(fieldSystem, NULL);
         temp_r0->unk4++;
         break;
     case 13:
         if (FieldSystem_ApplicationIsRunning(fieldSystem) == 0) {
-            Heap_Free(temp_r0->unk10);
+            Heap_Free(temp_r0->overlayArgs1);
             temp_r0->unk4 = 2;
         }
         break;
@@ -180,13 +179,13 @@ BOOL sub_02078834(TaskManager* taskManager) {
         }
         break;
     case 16:
-        temp_r0->unk10 = sub_02078C18(fieldSystem, HEAP_ID_FIELD2, temp_r0->unk14);
+        temp_r0->overlayArgs1 = sub_02078C18(fieldSystem, HEAP_ID_FIELD2, temp_r0->unk14);
         temp_r0->unk4++;
         break;
     case 17:
         if (FieldSystem_ApplicationIsRunning(fieldSystem) == 0) {
             temp_r0->unk4 = 2;
-            sub_02078C60(temp_r0->unk10);
+            sub_02078C60(temp_r0->overlayArgs1);
         }
         break;
     case 18:
@@ -199,13 +198,13 @@ BOOL sub_02078834(TaskManager* taskManager) {
         }
         break;
     case 20:
-        temp_r0->unk10 = sub_02078C74(fieldSystem, HEAP_ID_FIELD2, temp_r0->unk14);
+        temp_r0->overlayArgs1 = sub_02078C74(fieldSystem, HEAP_ID_FIELD2, temp_r0->unk14);
         temp_r0->unk4++;
         break;
     case 21:
         if (FieldSystem_ApplicationIsRunning(fieldSystem) == 0) {
             temp_r0->unk4 = 2;
-            sub_02078CB4(temp_r0->unk10);
+            sub_02078CB4(temp_r0->overlayArgs1);
         }
         break;
     case 22:
@@ -218,13 +217,13 @@ BOOL sub_02078834(TaskManager* taskManager) {
         }
         break;
     case 24:
-        temp_r0->unk10 = sub_02078CC8(fieldSystem, HEAP_ID_FIELD2, temp_r0->unk14);
+        temp_r0->overlayArgs1 = sub_02078CC8(fieldSystem, HEAP_ID_FIELD2, temp_r0->unk14);
         temp_r0->unk4++;
         break;
     case 25:
         if (FieldSystem_ApplicationIsRunning(fieldSystem) == 0) {
             temp_r0->unk4 = 2;
-            sub_02078D10(temp_r0->unk10);
+            sub_02078D10(temp_r0->overlayArgs1);
         }
         break;
     default:
@@ -233,66 +232,65 @@ BOOL sub_02078834(TaskManager* taskManager) {
     return 0;
 }
 
-UnkStruct_sub_02078834* sub_02078B2C(void) {
-    UnkStruct_sub_02078834* temp_r0 = Heap_AllocAtEnd(HEAP_ID_FIELD2, sizeof(UnkStruct_sub_02078834));
+UnkStruct_sub_02078834 *sub_02078B2C(void) {
+    UnkStruct_sub_02078834 *temp_r0 = Heap_AllocAtEnd(HEAP_ID_FIELD2, sizeof(UnkStruct_sub_02078834));
     MI_CpuFill8(temp_r0, 0, sizeof(UnkStruct_sub_02078834));
-    UnkStruct_sub_02079352* temp_r0_2 = Heap_AllocAtEnd(HEAP_ID_FIELD2, sizeof(UnkStruct_sub_02079352));
-    temp_r0->unk0 = temp_r0_2;
-    MI_CpuFill8(temp_r0_2, 0, sizeof(UnkStruct_sub_02079352));
+    UnkStruct_ov44_0222A758_args *temp_r0_2 = Heap_AllocAtEnd(HEAP_ID_FIELD2, sizeof(UnkStruct_ov44_0222A758_args));
+    temp_r0->overlayArgs0 = temp_r0_2;
+    MI_CpuFill8(temp_r0_2, 0, sizeof(UnkStruct_ov44_0222A758_args));
     return temp_r0;
 }
 
-void sub_02078B58(TaskManager* taskManager) {
-    UnkStruct_sub_02078834* temp_r0 = sub_02078B2C();
-    temp_r0->unk0->unk4 = 2;
+void sub_02078B58(TaskManager *taskManager) {
+    UnkStruct_sub_02078834 *temp_r0 = sub_02078B2C();
+    temp_r0->overlayArgs0->unk4 = 2;
     TaskManager_Call(taskManager, sub_02078834, temp_r0);
 }
 
-void sub_02078B78(TaskManager* taskManager, u16* arg1) {
-    UnkStruct_sub_02078834* temp_r0 = sub_02078B2C();
-    temp_r0->unk0->unk4 = 1;
+void sub_02078B78(TaskManager *taskManager, u16 *arg1) {
+    UnkStruct_sub_02078834 *temp_r0 = sub_02078B2C();
+    temp_r0->overlayArgs0->unk4 = 1;
     temp_r0->unk8 = arg1;
     TaskManager_Call(taskManager, sub_02078834, temp_r0);
 }
 
-void sub_02078B9C(UnkStruct_sub_02078834* arg0, FieldSystem* fieldSystem, enum HeapID arg2, s32 arg3) {
-    UnkStruct_sub_02078B9C* temp_r0 = Heap_Alloc(arg2, 0xC);
+void sub_02078B9C(UnkStruct_sub_02078834 *arg0, FieldSystem *fieldSystem, enum HeapID arg2, s32 arg3) {
+    UnkStruct_ov44_args *temp_r0 = Heap_Alloc(arg2, 0xC);
     temp_r0->unk0 = arg3;
     temp_r0->unk1 = 2;
     temp_r0->unk2 = _0210125C[arg3];
     temp_r0->unk3 = 0;
     temp_r0->unk4 = 0;
     temp_r0->unk8 = fieldSystem->saveData;
-    arg0->unk10 = temp_r0;
+    arg0->overlayArgs1 = temp_r0;
     FieldSystem_LaunchApplication(fieldSystem, &_02101280, temp_r0);
 }
 
-s32 sub_02078BD8(UnkStruct_sub_02078834* arg0) {
-    UnkStruct_sub_02078B9C* temp_r0 = arg0->unk10;
+s32 sub_02078BD8(UnkStruct_sub_02078834 *arg0) {
+    UnkStruct_ov44_args *temp_r0 = arg0->overlayArgs1;
     if (temp_r0->unk3 == 1) {
         switch (temp_r0->unk0) {
         case 1:
-            arg0->unk4 = 0x10;
+            arg0->unk4 = 16;
             break;
         case 2:
-            arg0->unk4 = 0x14;
+            arg0->unk4 = 20;
             break;
         case 3:
         default:
-            arg0->unk4 = 0x18;
+            arg0->unk4 = 24;
             break;
         }
     } else {
         arg0->unk4 = 1;
     }
     arg0->unk14 = temp_r0->unk4;
-    Heap_Free(arg0->unk10);
+    Heap_Free(arg0->overlayArgs1);
     return arg0->unk4;
 }
 
-UnkStruct_sub_02078C18* sub_02078C18(FieldSystem* fieldSystem, enum HeapID arg1, s32 arg2) {
-
-    UnkStruct_sub_02078C18* temp_r0 = Heap_Alloc(arg1, 0x3C);
+UnkStruct_sub_02078C18 *sub_02078C18(FieldSystem *fieldSystem, enum HeapID arg1, s32 arg2) {
+    UnkStruct_sub_02078C18 *temp_r0 = Heap_Alloc(arg1, 0x3C);
     memset(temp_r0, 0, 0x3C);
     temp_r0->unk38 = arg2;
     temp_r0->unk39 = 0;
@@ -302,13 +300,13 @@ UnkStruct_sub_02078C18* sub_02078C18(FieldSystem* fieldSystem, enum HeapID arg1,
     return temp_r0;
 }
 
-void sub_02078C60(void* arg0) {
+void sub_02078C60(void *arg0) {
     Heap_Free(arg0);
     UnloadOverlayByID(FS_OVERLAY_ID(OVY_90));
 }
 
-UnkStruct_sub_02078C74* sub_02078C74(FieldSystem* fieldSystem, enum HeapID arg1, s32 arg2) {
-    UnkStruct_sub_02078C74* temp_r0 = Heap_Alloc(arg1, 0x40);
+UnkStruct_sub_02078C74 *sub_02078C74(FieldSystem *fieldSystem, enum HeapID arg1, s32 arg2) {
+    UnkStruct_sub_02078C74 *temp_r0 = Heap_Alloc(arg1, 0x40);
     memset(temp_r0, 0, 0x40);
     temp_r0->unk3C = arg2;
     temp_r0->unk38 = 0;
@@ -318,13 +316,13 @@ UnkStruct_sub_02078C74* sub_02078C74(FieldSystem* fieldSystem, enum HeapID arg1,
     return temp_r0;
 }
 
-void sub_02078CB4(void* arg0) {
+void sub_02078CB4(void *arg0) {
     Heap_Free(arg0);
     UnloadOverlayByID(FS_OVERLAY_ID(OVY_90));
 }
 
-UnkStruct_sub_02078C18* sub_02078CC8(FieldSystem* fieldSystem, enum HeapID arg1, s32 arg2) {
-    UnkStruct_sub_02078C18* temp_r0 = Heap_Alloc(arg1, 0x3CU);
+UnkStruct_sub_02078C18 *sub_02078CC8(FieldSystem *fieldSystem, enum HeapID arg1, s32 arg2) {
+    UnkStruct_sub_02078C18 *temp_r0 = Heap_Alloc(arg1, 0x3C);
     MI_CpuFill8(temp_r0, 0, 0x3C);
     temp_r0->unk38 = arg2;
     temp_r0->unk39 = 0;
@@ -334,13 +332,13 @@ UnkStruct_sub_02078C18* sub_02078CC8(FieldSystem* fieldSystem, enum HeapID arg1,
     return temp_r0;
 }
 
-void sub_02078D10(void* arg0) {
+void sub_02078D10(void *arg0) {
     Heap_Free(arg0);
     UnloadOverlayByID(FS_OVERLAY_ID(OVY_90));
 }
 
 s32 sub_02078D24(s32 arg0) {
-    UnkStruct_ov44_0223197C* tempStruct = sub_020398C8();
+    UnkStruct_ov44_0223197C *tempStruct = sub_020398C8();
     u8 temp_r2 = tempStruct->unk0.unk1B;
     u8 temp_r0 = tempStruct->unk24[arg0].unk1B;
     if (temp_r2 == 12 && temp_r0 == 5) {
